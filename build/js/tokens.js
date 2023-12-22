b85b633fb498db9c6d419958746e428579d6d5ed
Arthur Fakhouri <arthurfakhouri@outlook.com>
2023-12-22 16:19:09 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 22 Dec 2023 19:13:15 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray500": "#a1a1a1",
+  "ColorGray700": "#1c1c1f",
+  "ColorGray800": "#1f1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
